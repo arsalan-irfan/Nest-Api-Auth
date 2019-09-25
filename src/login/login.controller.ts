fc53582990266df565209125c8048da579d59255
Arsalan <arsalanirfan888@gmai.com>
2019-09-25 19:51:54 +0500

    Jwt implemented

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller,Get, Request, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import {AuthService} from '../auth/auth.service'
 
-@Controller('api/login')
+@Controller('api')
 export class LoginController {
+  constructor(private readonly authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
-  @Post()
+  @Post('login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
