2e0bbd169c31ac574323bfac4c21421a2509642d
Arsalan <mohdarsalan318@gmail.com>
2019-10-01 00:48:44 +0500

    Finalizing Client

---
+++ todo-client/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {TodosComponent} from './todos/todos.component'
+
+const routes: Routes = [
+  {path:'',component:TodosComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
